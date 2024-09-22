76ccd2421d45eaaaf9ec6f8c016b15e94855f1c0
mrnetbear <akonotop03@mail.ru>
2024-09-22 23:24:02 +0300

    added different methods of error analysis

--- nmTask1.cpp
+++ nmTask1.cpp
@@ -20,12 +20,22 @@
 #include "TDecompLU.h"
 
 #define NUM_OF_NODS 7
+#define NUM_OF_NODS_ACC 8
 #define LEFT_NOD 0
 #define RIGHT_NOD 3
 #define NUM_OF_POINTS 100
+//#define M_PI 3.1415926535
 
 
-void basedPoly(TMatrixD &x, TMatrixD &f, double *x1, double *fx2/*const double (&x1)[NUM_OF_POINTS], double (&fx2)[NUM_OF_POINTS]*/){
+int factorial(int n){
+    int res = 1;
+    for (int i=1; i<n; i++){
+        res *= i;
+    }
+    return res;
+}
+
+void basedPoly(TMatrixD &x, TMatrixD &f, double *x1, double *fx2){
     //Define the coefficients
     TMatrixD A(NUM_OF_NODS,NUM_OF_NODS);
     TMatrixD aj(NUM_OF_NODS,1);
@@ -43,13 +53,16 @@ void basedPoly(TMatrixD &x, TMatrixD &f, double *x1, double *fx2/*const double (
     aj = A.Invert(&det)*f;
     
     for (int i = 0; i < NUM_OF_POINTS; i++){
+        fx2[i] = 0.0;
         for (int j = 0; j < NUM_OF_NODS; j++){
             fx2[i] += aj(j,0)*pow(x1[i], j);
+            //std::cout << "aj[" << j << "] = " <<aj(j,0) << ";" << std::endl;
         }
+        //std::cout << "f[" << i << "] = " << fx2[i] << ";" << std::endl;
     }
 }
 
-void lagrangePoly(TMatrixD &x, TMatrixD &f,  double *x1, double *fx3/*const double (&x1)[NUM_OF_POINTS], double (&fx3)[NUM_OF_POINTS]*/){
+void lagrangePoly(TMatrixD &x, TMatrixD &f,  double *x1, double *fx3){
     //Calculate Lagrange coefficients
     double lagrangeCoef[NUM_OF_NODS];
     for (int i = 0; i < NUM_OF_NODS; i++) {
@@ -75,6 +88,52 @@ void lagrangePoly(TMatrixD &x, TMatrixD &f,  double *x1, double *fx3/*const doub
     }
 }
 
+void newtonPoly(TMatrixD &x, TMatrixD &f,  double *x1, double *fx4, int num){
+    //Calculate divided differences
+    double dividedDifferences[num][num];
+    for (int i = 0; i < num; i++) {
+        dividedDifferences[i][0] = f(i, 0);
+    }
+    for (int i = 0; i < num; i++){
+        for (int j =1; j <= i; j++){
+            dividedDifferences[i][j] = (dividedDifferences[i][j-1] - dividedDifferences[i-1][j-1])/(x(i,0)-x(i-j,0));
+            //std::cout << " F[" << i << "," << j << "] = " << dividedDifferences[i][j];
+        }
+    }
+
+    //Calculate Newton polynomial
+    for (int i = 0; i < NUM_OF_POINTS; i++){
+        fx4[i] = 0.0;
+        for (int j = 0; j < num; j++){
+            double numerator = 1.0;
+            for (int k = 0; k < j; k++){
+                numerator *= (x1[i] - x(k,0));
+            }
+            fx4[i] += numerator * dividedDifferences[j][j];  
+        }
+        //std::cout << "f4[" << i << "] = " << fx4[i] << "; x4[" << i << "] = " << x1[i] << std::endl;
+    }
+}
+
+void errorEvaluation (TMatrixD &x, TMatrixD &f, double *x1, double *R, int num){
+    //Calculate coefficient
+    double coefR = 16.0 * sin(M_PI/4.0)*exp(-M_PI/4.0) / factorial(num);
+    
+    //Calculate R
+    for (int i = 0; i < NUM_OF_POINTS; i++){
+        R[i] = 0.0;
+        for (int j = 0; j < num; j++){
+            double numerator = 1.0;
+            for (int k = 0; k < j; k++){
+                numerator *= (x1[i] - x(k,0));
+            }
+            R[i] += numerator * coefR;  
+        }
+        //std::cout << "f4[" << i << "] = " << fx4[i] << "; x4[" << i << "] = " << x1[i] << std::endl;
+    }
+
+}
+
 int interPoly(){
 
     //Define the nods of the polynom
@@ -86,22 +145,6 @@ int interPoly(){
         x(i,0) = (double)RIGHT_NOD/(double)NUM_OF_NODS * i;
         f(i,0) = sin(x(i,0))*exp(-x(i,0));
     }
-
-    //Calculate divided differences
-    double dividedDifferences[NUM_OF_NODS][NUM_OF_NODS];
-    for (int i = 0; i < NUM_OF_NODS; i++) {
-        dividedDifferences[i][0] = f(i, 0);
-    }
-    for (int i = 0; i < NUM_OF_NODS; i++){
-        for (int j =1; j <= i; j++){
-            dividedDifferences[i][j] = (dividedDifferences[i][j-1] - dividedDifferences[i-1][j-1])/(x(i,0)-x(i-j,0));
-            std::cout << " F[" << i << "," << j << "] = " << dividedDifferences[i][j];
-        }
-    }
-
-    std::cout << std::endl;
-    std::cout << "=====================================================" << std::endl;
-
 
     //Generate original function
     double x1 [NUM_OF_POINTS];
@@ -122,26 +165,38 @@ int interPoly(){
     lagrangePoly(x, f, x1, fx3);
     
     //Calculate Newton polynomial
-
-    double x4 [NUM_OF_POINTS];
     double fx4 [NUM_OF_POINTS];
-    for (int i = 0; i < NUM_OF_POINTS; i++){
-        x4[i] = (double)RIGHT_NOD/(double)NUM_OF_POINTS * i;
-        for (int j = 0; j < NUM_OF_NODS; j++){
-            double numerator = 1.0;
-            for (int k = 0; k < j; k++){
-                numerator *= (x4[i] - x(k,0));
-            }
-            fx4[i] += numerator * dividedDifferences[j][j];  
-        }
-        std::cout << "f4[" << i << "] = " << fx4[i] << "; x4[" << i << "] = " << x4[i] << std::endl;
-    }
+    newtonPoly(x, f, x1, fx4, NUM_OF_NODS);
+    
 
     //Calculate difference between original and newton values
     double fdiff[NUM_OF_POINTS];
     for (int i = 0; i < NUM_OF_POINTS; i++){
         fdiff[i] = abs(fx4[i] - fx1[i]);
     }
+
+    TMatrixD fmore(NUM_OF_NODS_ACC, 1);
+    TMatrixD xmore(NUM_OF_NODS_ACC, 1);
+
+
+    for (int i=0; i<NUM_OF_NODS_ACC; i++){
+        xmore(i,0) = (double)RIGHT_NOD/(double)NUM_OF_NODS_ACC * i;
+        fmore(i,0) = sin(xmore(i,0))*exp(-xmore(i,0));
+        std::cout << "xmore[" << i << "] = " << x(i,0) << std::endl;
+        std::cout << "fmore[" << i << "] = " << fmore(i,0) << std::endl;
+    }
+
+    double fx5 [NUM_OF_POINTS];
+    newtonPoly(xmore, fmore, x1, fx5, NUM_OF_NODS_ACC);
+
+    double fdiffmore[NUM_OF_POINTS];
+    for (int i = 0; i < NUM_OF_POINTS; i++){
+        fdiffmore[i] = abs(fx5[i] - fx1[i]);
+    }
+
+    //Calculate R
+    double R[NUM_OF_POINTS];
+    errorEvaluation(xmore, fmore, x1, R, NUM_OF_NODS_ACC);
 
     //Draw original function
     TGraph* origin = new TGraph (NUM_OF_POINTS, x1, fx1);
@@ -167,9 +222,16 @@ int interPoly(){
     //Draw Newton Polynomial
     TGraph* approxNewton = new TGraph (NUM_OF_POINTS, x1, fx4);
     approxNewton->SetMarkerStyle(50);
-    approxNewton->SetMarkerColor(kOrange);
-    approxNewton->SetLineColor(kOrange);
+    approxNewton->SetMarkerColor(kBlack);
+    approxNewton->SetLineColor(kBlack);
     approxNewton->SetLineWidth(3);
+
+    //Draw Newton Polynomial
+    TGraph* approxNewtonMore = new TGraph (NUM_OF_POINTS, x1, fx5);
+    approxNewtonMore->SetMarkerStyle(50);
+    approxNewtonMore->SetMarkerColor(kGray);
+    approxNewtonMore->SetLineColor(kGray);
+    approxNewtonMore->SetLineWidth(1);
 
     //Draw difference between original and newton values
     TGraph* diff = new TGraph (NUM_OF_POINTS, x1, fdiff);
@@ -178,10 +240,22 @@ int interPoly(){
     diff->SetLineColor(kBlack);
     diff->SetLineWidth(2);
 
-    TCanvas* canvas1 = new TCanvas("canvas1", "Graph and approximations", 600, 900);
-    canvas1->Divide(1, 2);
-    canvas1->cd(1);
-    gPad->SetGrid();
+    //Draw difference between original and more newton values
+    TGraph* diffmore = new TGraph (NUM_OF_POINTS, x1, fdiffmore);
+    diffmore->SetMarkerStyle(60);
+    diffmore->SetMarkerColor(kGreen);
+    diffmore->SetLineColor(kGreen);
+    diffmore->SetLineWidth(2);
+
+    //Draw R
+    TGraph* error = new TGraph (NUM_OF_POINTS, x1, R);
+    error->SetMarkerStyle(60);
+    error->SetMarkerColor(kBlue);
+    error->SetLineColor(kBlue);
+    error->SetLineWidth(2);
+
+    TCanvas* canvas1 = new TCanvas("canvas1", "Graph and approximations", 900, 600);
+    canvas1->SetGrid();
 
     TLegend *legend = new TLegend(0.2, 0.2, 0.5, 0.5);
     legend->SetHeader("Interpolation Functions","C");
@@ -195,9 +269,34 @@ int interPoly(){
     approxLag->Draw("same");
     approxNewton->Draw("same");
     legend->Draw();
-
-    canvas1->cd(2);
+    
+    TCanvas* canvas2 = new TCanvas("canvas2", "Newton approximations & errors", 1200, 900);
+    canvas2->Divide(1, 2);
+    canvas2->cd(1);
     gPad->SetGrid();
+    TLegend *legend3 = new TLegend(0.2, 0.2, 0.5, 0.5);
+    legend3->SetHeader("Newtons Interpolations","C");
+    legend3->AddEntry(origin, "original", "lp");
+    legend3->AddEntry(approxNewton, "Newton Polynomial n = 7", "lp");
+    legend3->AddEntry(approxNewtonMore, "Newton Polynomial n = 8", "lp");
+
+    origin->Draw();
+    approxNewton->Draw("same");
+    approxNewtonMore->Draw("same");
+    legend3->Draw();
+
+
+    canvas2->cd(2);
+    TLegend *legend2 = new TLegend(0.2, 0.2, 0.5, 0.5);
+    legend2->SetHeader("Method error","C");
+    legend2->AddEntry(diff, "Difference n = 7", "lp");
+    legend2->AddEntry(diffmore, "Difference n = 8", "lp");
+    //legend2->AddEntry(error, "Error R" , "lp");
+    gPad->SetGrid();
+
     diff->Draw("AL");
+    diffmore->Draw("same");
+    //error->Draw("same");
+    legend2->Draw();
     return 0;
 }
