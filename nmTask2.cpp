18bad00516ec2bcf80017ad3d4f396174a0f54b9
Alexey Konotop <akonotop03@mail.ru>
2024-10-18 23:06:16 +0300

    added spline3 interpolation

--- nmTask2.cpp
+++ nmTask2.cpp
@@ -26,7 +26,8 @@
 #define NUM_OF_POINTS 100
 
 void fillSplineMatrix(TMatrixD &spline, double *h, int num);
-void splineInterpolation(TMatrixD &x, TMatrixD &f, double *x1, double *S, int num);
+void fillSpline3Matrix(TMatrixD &spline3, double *h, int num);
+void splineInterpolation(TMatrixD &x, TMatrixD &f, double *x1, double *S3, int num);
 
 void fillSplineMatrix(TMatrixD &spline, double *h, int num){
     //Fill the spline matrix
@@ -59,6 +60,42 @@ void fillSplineMatrix(TMatrixD &spline, double *h, int num){
     spline(num-1,num-1) = 1.0;
     std::cout << spline(num-1,num-1) << std::endl;
 }
+
+void fillSpline3Matrix(TMatrixD &spline3, double *h, int num){
+    //Fill the spline3 matrix
+    //Fill the first row
+    spline3(0,0) = 1/h[0];
+    spline3(0,1) = -1/h[0] - 1/h[1];
+    spline3(0,2) = 1/h[1];
+    std::cout << spline3(0,0) << "; " << spline3(0,1) << "; " << spline3(0,2);
+    for (int i = 3; i < num; i++){
+        spline3(0,i) = 0;
+        std::cout << "; " << spline3(0,i);
+    }
+    std::cout << std::endl;
+    //Fill center
+    for(int i = 1; i < num-1; i++){
+        for (int j = 0; j < num; j++){
+            if (j == i-1) spline3(i,j) = h[j];
+            else if (j == i) spline3(i,j) = 2 * (h[j-1] + h[j]);
+            else if (j == i+1) spline3(i,j) = h[j-1];
+            else spline3(i,j) = 0.0;
+            std::cout << spline3(i,j);
+            if (j<num-1) std::cout << "; ";
+        }
+        std::cout << std::endl;
+    }
+    //Fill the last row
+    for (int i = 0; i < num-3; i++){
+        spline3(num-1,i) = 0;
+        std::cout << spline3(num-1,i) << "; ";
+    }
+    spline3(num-1,num-3) = 1/h[num-3];
+    spline3(num-1,num-2) = -1/h[num-2] - 1/h[num-3];
+    spline3(num-1,num-1) = 1/h[num-2];
+    std::cout << spline3(num-1,num-3) << "; " << spline3(num-1,num-2) << "; " << spline3(num-1,num-1);
+    std::cout << std::endl;
+}
 void splineInterpolation(TMatrixD &x, TMatrixD &f, double *x1, double *S, int num){
 
     //Calculate the function differences
@@ -87,7 +124,6 @@ void splineInterpolation(TMatrixD &x, TMatrixD &f, double *x1, double *S, int nu
         b(i,0) = (f(i+1,0) - f(i,0)) / h[i] - h[i] * (2.0 * cVector(i,0) + cVector(i+1,0)) / 3.0;
         d(i,0) = (cVector(i+1,0) - cVector(i,0)) / (3.0 * h[i]);
     }
-
     //Calculate the spline values
     int k = 1;
     for (int i = 0; i < NUM_OF_POINTS; i++){
@@ -96,6 +132,47 @@ void splineInterpolation(TMatrixD &x, TMatrixD &f, double *x1, double *S, int nu
         }
         int j = k-1;
         S[i] = f(j,0) + b(j,0)*(x1[i] - x(j,0)) + cVector(j,0)*(x1[i] - x(j,0))*(x1[i] - x(j,0)) + d(j,0)*(x1[i] - x(j,0))*(x1[i] - x(j,0))*(x1[i] - x(j,0));
+    }
+}
+void spline3Interpolation(TMatrixD &x, TMatrixD &f, double *x1, double *S3, int num){
+    double h[num-1];
+    for (int i = 0; i < num-1; i++){
+        h[i] = x(i+1,0) - x(i,0);
+    }
+    //Calculate the spline matrix
+    TMatrixD spline3(num, num);
+    fillSpline3Matrix(spline3, h, num);
+
+    //calculate the devided differences
+    TMatrixD divDiff(num, 1);
+    divDiff(0,0) = 0;
+    divDiff(num-1,0) = 0;
+    for (int i = 1; i < num-1; i++){
+        divDiff(i,0) = 3.0 * ((f(i+1,0) - f(i,0)) / h[i] - (f(i,0) - f(i-1,0))/h[i-1]);
+    }
+
+    //Calculate the C coefficients
+    TMatrixD cVector = spline3.Invert() * divDiff;
+    //Calculate the B & D coefficients
+    TMatrixD b(num-1,1);
+    TMatrixD d(num-1,1);
+    for (int i = 0; i < num-1; i++){
+        b(i,0) = (f(i+1,0) - f(i,0)) / h[i] - h[i] * (2.0 * cVector(i,0) + cVector(i+1,0)) / 3.0;
+        if (i == 1 || i == num-1){
+            d[i] = d[i-1];
+            continue;
+        }
+        d(i,0) = (cVector(i+1,0) - cVector(i,0)) / (3.0 * h[i]);
+    }
+    std::cout << "Calculation of B&&D coefficients completed" << std::endl;
+     //Calculate the spline values
+    int k = 1;
+    for (int i = 0; i < NUM_OF_POINTS; i++){
+        if (k < num && x1[i] > x(k,0)){
+            k++;
+        }
+        int j = k-1;
+        S3[i] = f(j,0) + b(j,0)*(x1[i] - x(j,0)) + cVector(j,0)*(x1[i] - x(j,0))*(x1[i] - x(j,0)) + d(j,0)*(x1[i] - x(j,0))*(x1[i] - x(j,0))*(x1[i] - x(j,0));
     }
 }
 
@@ -127,10 +204,21 @@ int interPoly(){
     double S[NUM_OF_POINTS];
     splineInterpolation(x, f, x1, S, NUM_OF_NODS);
 
+    std::cout << std::endl;
+    //Calculate spline3 interpolation
+    double S3[NUM_OF_POINTS];
+    spline3Interpolation(x, f, x1, S3, NUM_OF_NODS);
+
     //Calculate difference between original and spline values
     double fdiffspline[NUM_OF_POINTS];
     for (int i = 0; i < NUM_OF_POINTS; i++){
         fdiffspline[i] = abs(S[i] - fx1[i]);
+    }
+
+    //Calculate difference between original and spline3 values
+    double fdiffspline3[NUM_OF_POINTS];
+    for (int i = 0; i < NUM_OF_POINTS; i++){
+        fdiffspline3[i] = abs(S3[i] - fx1[i]);
     }
 
     //Draw nodes
@@ -165,6 +253,15 @@ int interPoly(){
     spline->SetLineStyle(1);
     spline->SetLineWidth(2);
 
+    //Draw spline3 interpolation
+    TGraph* spline3 = new TGraph (NUM_OF_POINTS, x1, S3);
+    spline3->SetMarkerStyle(22);
+    spline3->SetMarkerSize(0.1);
+    spline3->SetMarkerColor(kBlue);
+    spline3->SetLineColor(kBlue);
+    spline3->SetLineStyle(2);
+    spline3->SetLineWidth(2);
+
     //Draw difference between original and spline values
     TGraph* diffspline = new TGraph (NUM_OF_POINTS, x1, fdiffspline);
     diffspline->SetMarkerStyle(60);
@@ -173,17 +270,47 @@ int interPoly(){
     diffspline->SetLineStyle(1);
     diffspline->SetLineWidth(2);
 
+    //Draw difference between original and spline3 values
+    TGraph* diffspline3 = new TGraph (NUM_OF_POINTS, x1, fdiffspline3);
+    diffspline3->SetMarkerStyle(60);
+    diffspline3->SetMarkerColor(kBlue);
+    diffspline3->SetLineColor(kBlue);
+    diffspline3->SetLineStyle(2);
+    diffspline3->SetLineWidth(2);
+
+    //Draw all
+
     TCanvas *canvas1 = new TCanvas("canvas4", "Spline interpolation", 1200, 900);
     canvas1->Divide(1, 2);
     canvas1->cd(1);
     gPad->SetGrid();
+
+    TLegend* leg1 = new TLegend(0.1, 0.7, 0.3, 0.9);
+    leg1->SetFillColor(0);
+    leg1->AddEntry(origin, "Original function", "L");
+    leg1->AddEntry(spline, "Spline interpolation", "L");
+    leg1->AddEntry(spline3, "Spline3 interpolation", "L");
+
+
     origin->Draw();
     nodes7->Draw("Psame");
     spline->Draw("same");
+    spline3->Draw("same");
+
+    leg1->Draw("same");
 
     canvas1->cd(2);
     gPad->SetGrid();
+
+    TLegend* leg2 = new TLegend(0.1, 0.7, 0.3, 0.9);
+    leg2->SetFillColor(0);
+    leg2->AddEntry(diffspline, "Spline Error", "L");
+    leg2->AddEntry(diffspline3, "Spline3 Error", "L");
+
     diffspline->Draw("AL");
+    diffspline3->Draw("same");
+
+    leg2->Draw("same");
 
     return 0;
 }
