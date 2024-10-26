7055d7f9c32664054bee3b05409e015a81a3d92a
Alexey Konotop <akonotop03@mail.ru>
2024-10-26 12:16:33 +0300

    began works in numerical diff

---
+++ nmTask3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <time.h>
+#include <random>
+#include "TFile.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TTree.h"
+#include "TRandom.h"
+#include "TCanvas.h"
+#include "TGraph.h"
+#include "TLegend.h"
+#include "TLatex.h"
+#include "TMatrixD.h"
+#include "TDecompLU.h"
+
+
+int numDiff(){
+    
+    return 0;
+}
