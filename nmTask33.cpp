75b95c309bb1945562d90c3f075f2348c5fda98f
mrnetbear <akonotop03@mail.ru>
2024-12-16 18:15:57 +0300

    added full solution for task 3.3

--- nmTask33.cpp
+++ nmTask33.cpp
@@ -61,50 +61,99 @@ void trapezoidIntegrator(double *result, double stepSize){
         
 }
 
+void GKIntegrator2(double *C, double *adX1, double stepSize){
+    double  C0 = 0,
+            C1 = 0;
+    for(double xi = stepSize; xi <= 1; xi += stepSize){
+        double h = qGrid(xi) - qGrid(xi-stepSize);
+        C0 += (qGrid(xi-stepSize) + 0.5*h - adX1[1])/(adX1[0] - adX1[1])*f(qGrid(xi-stepSize)+0.5*h)*h;
+        C1 += (qGrid(xi-stepSize) + 0.5*h - adX1[0])/(adX1[1] - adX1[0])*f(qGrid(xi-stepSize)+0.5*h)*h;
+    }
+    C[0] = C0;
+    C[1] = C1;
+}
+
+void GKIntegrator3(double *C, double *adX2, double stepSize){
+    double  C0 = 0,
+            C1 = 0,
+            C2 = 0;
+    for(double xi = stepSize; xi <= 1; xi += stepSize){
+        double h = qGrid(xi) - qGrid(xi-stepSize);
+        C0 += (qGrid(xi-stepSize) + 0.5*h - adX2[1]) * (qGrid(xi-stepSize) + 0.5*h - adX2[2]) / (adX2[0] - adX2[1]) / (adX2[0] - adX2[2])*f(qGrid(xi-stepSize)+0.5*h)*h;
+        C1 += (qGrid(xi-stepSize) + 0.5*h - adX2[0]) * (qGrid(xi-stepSize) + 0.5*h - adX2[2]) / (adX2[1] - adX2[0]) / (adX2[1] - adX2[2])*f(qGrid(xi-stepSize)+0.5*h)*h;
+        C2 += (qGrid(xi-stepSize) + 0.5*h - adX2[0]) * (qGrid(xi-stepSize) + 0.5*h - adX2[1]) / (adX2[2] - adX2[0]) / (adX2[2] - adX2[1])*f(qGrid(xi-stepSize)+0.5*h)*h;
+    }
+    C[0] = C0;
+    C[1] = C1;
+    C[2] = C2;
+}
+
 void numIntMore(){
 
     //define step size
-    const double stepSize = 1e-5;
+    const double fStepSize = 1e-5;
 
     //define accurate integral value
-    const double accInt = 1;
+    const double dAccInt = 1;
 
-    double ctrInt, trpInt;
-    std::thread th1(centralIntegrator, &ctrInt, stepSize);
-    std::thread th2(trapezoidIntegrator, &trpInt, stepSize);
+    double dCtrInt, dTrpInt;
+    std::thread th1(centralIntegrator, &dCtrInt, fStepSize);
+    std::thread th2(trapezoidIntegrator, &dTrpInt, fStepSize);
     th1.join();
     th2.join();
 
-    double ctrErr[10000], trpErr[10000];
-    double h1[10000];
+    double adCtrErr[10000], adTrpErr[10000];
+    double adH1[10000];
     size_t i = 0;
-    for (double h = stepSize; h < 1e-1 || i < 10000; h += stepSize){
-        h1[i] = h;
+    for (double h = fStepSize; h < 1e-1 || i < 10000; h += fStepSize){
+        adH1[i] = h;
         double ctr, trp;
         std::thread th1(centralIntegrator, &ctr, h);
         std::thread th2(trapezoidIntegrator, &trp, h);
         th1.join();
         th2.join();
-        ctrErr[i] = abs(accInt - ctr);
-        trpErr[i] = abs(accInt - trp);
+        adCtrErr[i] = abs(dAccInt - ctr);
+        adTrpErr[i] = abs(dAccInt - trp);
         i++;
     }
 
-    std::cout << "Central Rectangles Integration Result: " << ctrInt << std::endl;
-    std::cout << "Trapezoidal Rule Integration Result: " << trpInt << std::endl;
+    //define Gauss-Kristoffel algorithm
+    const int   iNum0 = 1,
+                iNum1 = 2,
+                iNum2 = 3;
+
+    double  adX0[iNum0] = {1},
+            adX1[iNum1] = {2 - sqrt(2), 2 + sqrt(2)},
+            adX2[iNum2] = {0.4158, 2.2942, 6.2899};
+    
+    double  adC0[iNum0] = {dCtrInt},
+            adC1[iNum1],
+            adC2[iNum2] = {0, 0, 0};
+
+    GKIntegrator2(adC1, adX1, fStepSize);
+    GKIntegrator3(adC2, adX2, fStepSize);
+
+    double  dGKInt2 = adC1[0] + adC1[1],
+            dGKInt3 = adC2[0] + adC2[1] + adC2[2];
+    
+    std::cout << "Central Rectangles Integration Result: " << dCtrInt << std::endl;
+    std::cout << "Trapezoidal Rule Integration Result: " << dTrpInt << std::endl;
+    std::cout << "Gauss-Kristoffel 0 Integration Result: " << adC0[0] << std::endl;
+    std::cout << "Gauss-Kristoffel 1 Integration Result: " << dGKInt2 << std::endl;
+    std::cout << "Gauss-Kristoffel 2 Integration Result: " << dGKInt3 << std::endl;
 
     TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
     c1->SetGrid();
     c1->SetLogx();
     c1->SetLogy();
 
-    TGraph *gr1 = new TGraph(i, h1, ctrErr);
+    TGraph *gr1 = new TGraph(i, adH1, adCtrErr);
     gr1->SetTitle("Central Rectangles Error vs Step Size");
     gr1->SetLineStyle(5);
     gr1->SetLineWidth(6);
     gr1->SetLineColor(kRed);
 
-    TGraph *gr2 = new TGraph(i, h1, trpErr);
+    TGraph *gr2 = new TGraph(i, adH1, adTrpErr);
     gr2->SetTitle("Trapezoidal Rule Error vs Step Size");
     gr2->SetLineStyle(2);
     gr2->SetLineWidth(6);
